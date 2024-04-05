71d8ba0a0461d8f4e39ff448464406541ef5cfaa
Athul Anoop <athulanoop2020@gmail.com>
2024-04-05 20:59:42 +0530

    Update projects-controller.js
    Commented buggy line of code which prevented the project data from being saved to the database

--- server/src/controllers/projects-controller.js
+++ server/src/controllers/projects-controller.js
@@ -4,96 +4,97 @@ import fetchRepoData from "../utils/gitPull.js";
 import mongoose from "mongoose";
 
 const createProject = async (req, res) => {
-    const description = req.body.description;
-    const githubURL = req.body.projectURL;
-    const splitURL = githubURL.split("/");
-    const { readmeContent, contributors } = await fetchRepoData(
-        splitURL[3],
-        splitURL[4],
-    );
-    let querySTMT =
-        "give me a comma separated string containing all the tech stacks required by the project based on this abstract. I DO NOT want arrays, json objects, or any other fancy stuff. simple plain comma separated words corresponding to the tech stack for the project, if there are not tech stacks, just type ''";
-    const techStacks = await GemniPrompt(description, querySTMT);
-    console.log(techStacks);
-    req.body.techStacks = techStacks.split(", ");
-    querySTMT = "Provide a summary of the given text";
-    const summary = await GemniPrompt(
-        `${readmeContent} \n ${contributors}`,
-        querySTMT,
-    );
-    console.log("\n\n\n", summary);
-    req.body.summary = summary;
-    req.body.contributors = contributors;
-    req.body.readmeFile = readmeContent;
-    const Project = new ProjectModel(req.body);
-    console.log("\n\n\n", Project);
-    try {
-        const res_ = await Project.save();
-        console.log("\n \n \n", res_);
-        res.status(201).json(Project);
-    } catch (error) {
-        res.status(409).json({ message: error.message });
-    }
+  const description = req.body.description;
+  const githubURL = req.body.projectURL;
+  const splitURL = githubURL.split("/");
+  const { readmeContent, contributors } = await fetchRepoData(
+    splitURL[3],
+    splitURL[4],
+  );
+  let querySTMT =
+    "give me a comma separated string containing all the tech stacks required by the project based on this abstract. I DO NOT want arrays, json objects, or any other fancy stuff. simple plain comma separated words corresponding to the tech stack for the project, if there are not tech stacks, just type ''";
+  const techStacks = await GemniPrompt(description, querySTMT);
+  console.log(techStacks);
+  req.body.techStacks = techStacks.split(", ");
+  querySTMT = "Provide a summary of the given text";
+  const summary = await GemniPrompt(
+    `${readmeContent} \n ${contributors}`,
+    querySTMT,
+  );
+  console.log("\n\n\n", summary);
+  req.body.summary = summary;
+  // req.body.contributors = contributors ? contributors : [];
+  req.body.readmeFile = readmeContent;
+  const Project = new ProjectModel(req.body);
+  console.log("\n\n\n", Project);
+  try {
+    console.log("Saving");
+    const res_ = await Project.save();
+    console.log("Saved");
+    return res.status(201).json(res_);
+  } catch (error) {
+    return res.status(409).json({ message: error.message });
+  }
 };
 
-const getProjects = async function(req, res) {
-    const nameQuery = req.query.q || "";
-    const stackQuery = req.query.s || "";
-    const reName = new RegExp(nameQuery, "i");
-    const reStack = new RegExp(stackQuery, "i");
-    try {
-        const Projects = await ProjectModel.find({
-            $and: [{ name: reName }, { techStacks: reStack }],
-        });
-        res.status(200).json(Projects);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+const getProjects = async function (req, res) {
+  const nameQuery = req.query.q || "";
+  const stackQuery = req.query.s || "";
+  const reName = new RegExp(nameQuery, "i");
+  const reStack = new RegExp(stackQuery, "i");
+  try {
+    const Projects = await ProjectModel.find({
+      $and: [{ name: reName }, { techStacks: reStack }],
+    });
+    res.status(200).json(Projects);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
 const getProject = async (req, res) => {
-    const { id: _id } = req.params;
-    try {
-        const project = await ProjectModel.findById(_id);
+  const { id: _id } = req.params;
+  try {
+    const project = await ProjectModel.findById(_id);
 
-        res.status(200).json(project);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+    res.status(200).json(project);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
 const updateProject = async (req, res) => {
-    const { id: _id } = req.params;
-    const project = req.body;
+  const { id: _id } = req.params;
+  const project = req.body;
 
-    if (!mongoose.Types.ObjectId.isValid(_id))
-        return res.status(404).send("No project with that id");
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No project with that id");
 
-    try {
-        const updatedProject = await ProjectModel.findByIdAndUpdate(
-            _id,
-            { ...project, _id },
-            { new: true },
-        );
+  try {
+    const updatedProject = await ProjectModel.findByIdAndUpdate(
+      _id,
+      { ...project, _id },
+      { new: true },
+    );
 
-        res.json(updatedProject);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+    res.json(updatedProject);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
 const deleteProject = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id))
-        return res.status(404).send("No project with that id");
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No project with that id");
 
-    try {
-        await ProjectModel.findByIdAndDelete(id);
-        res.json({ message: "Project deleted successfully" });
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+  try {
+    await ProjectModel.findByIdAndDelete(id);
+    res.json({ message: "Project deleted successfully" });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
 export { createProject, getProjects, getProject, updateProject, deleteProject };
