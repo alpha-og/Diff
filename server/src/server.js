3662a84c9a956d6eab05d5f7625eb0a04524b0be
sharon504 <sharonsphajan@gmail.com>
2024-04-05 09:46:42 +0530

    Adding user backend

--- server/src/server.js
+++ server/src/server.js
@@ -6,6 +6,12 @@ dotenv.config({ path: "./configs.env" });
 const { PORT, MONGO_URL } = process.env;
 import connectDB from "./utils/connect-db.js";
 connectDB(MONGO_URL);
+
+// Testing - adding new users
+// import testDataCreate from "../test/makeUsers.js";
+// import dummyUser from "../test/data.js";
+//
+// testDataCreate(dummyUser);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} at http://localhost:${PORT}`);
 });
