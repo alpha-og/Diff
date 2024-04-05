aae6c06fc3f8dad69ba0ce3b5e162233053d1675
Athul Anoop <athulanoop2020@gmail.com>
2024-04-05 12:35:19 +0530

    Added projects page and list component with corresponding context provider

---
+++ client/src/components/components.js
@@ -0,0 +1,3 @@
+import ProjectsList from "./projects/ProjectsList.jsx";
+
+export { ProjectsList };
---
+++ client/src/pages/projects.jsx
@@ -0,0 +1,11 @@
+import { ProjectsList } from "../components/components.js";
+
+const ProjectsPage = () => {
+  return (
+    <div>
+      <ProjectsList />
+    </div>
+  );
+};
+
+export default ProjectsPage;
