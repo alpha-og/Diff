6d7d3002b1ae7cae90960e786d620fa4d9972576
Athul Anoop <athulanoop2020@gmail.com>
2024-04-05 12:35:54 +0530

    Implemented routing and wrapped the root element with ProjectsProvider component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Route, Routes } from "react-router-dom";
+import { ProjectsPage } from "./pages/pages.js";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/projects" element={<ProjectsPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
