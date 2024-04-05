847179e26af56e30ae3638d759319a96f7de8d3e
Athul Anoop <athulanoop2020@gmail.com>
2024-04-05 20:58:32 +0530

    Update components.js

--- client/src/components/components.js
+++ client/src/components/components.js
@@ -1,7 +1,8 @@
 import User from "./user.jsx";
 import SearchBar from "./projects/searchbar.jsx";
-import ProjectsList from "./projects/ProjectsList.jsx";
-import ProjectDetailView from "./projects/project-details.jsx";
+import ProjectsList from "./projects/projects-list.jsx";
+import ProjectDetailView from "./projects/project-details-view.jsx";
 import ProjectCreateForm from "./projects/create-project-form.jsx";
+import ProjectApplicationForm from "./projects/project-application-form.jsx";
 
-export { User, ProjectsList, SearchBar, ProjectDetailView, ProjectCreateForm };
+export { User, ProjectsList, SearchBar, ProjectDetailView, ProjectCreateForm, ProjectApplicationForm };
