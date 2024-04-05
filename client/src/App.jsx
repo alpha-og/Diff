2bf2b8a8ec499a944f37f476d8aba2e73d31d91d
sharon504 <sharonsphajan@gmail.com>
2024-04-05 12:47:08 +0530

    Added user profile ui components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { User } from "./components/components.js";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <User id="660f77dd191cb0e2c20d59b0" />
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/components/components.js
@@ -0,0 +1,3 @@
+import User from "./user.jsx";
+
+export { User };
